8ab5f63f1f7e0cebe4a626d570ec352f0a755677
Nurik <nicksbastion@yahoo.com>
2023-10-22 23:08:42 -0400

    @US26 added

--- src/test/java/pages/HealthSeniorPage.java
+++ src/test/java/pages/HealthSeniorPage.java
@@ -1,10 +1,24 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utils.BrowserUtils;
+
+import java.util.List;
 
 public class HealthSeniorPage {
     public HealthSeniorPage() {
         PageFactory.initElements(BrowserUtils.getDriver(), this);
     }
+    @FindBy(xpath = "//div[@id='parent-padding-654710006']//div[@class='col-12 col-12 col-xs-12 col-sm-12 col-md-12 col-lg-4  ']")
+    public List<WebElement> pictures;
+
+    @FindBy(xpath = "//div[@id='parent-padding-654710006']//div[@class='adt7-text-multi aem-GridColumn aem-GridColumn--default--12']")
+    public List<WebElement> textUnderPic;
+
+    @FindBy(xpath = "//div[@id='parent-padding-654710006']//div[@class='adt7-text-multi aem-GridColumn aem-GridColumn--default--12']")
+    public List<WebElement> onlineSpecialBtn;
+
 }
+//div[@id='parent-padding-654710006']//div[@class='adt7-text-multi txt-m-1']/div[@class='text-multi-1']/h5
--- src/test/java/step_definitions/HealthSeniorSteps.java
+++ src/test/java/step_definitions/HealthSeniorSteps.java
@@ -1,12 +1,53 @@
 package step_definitions;
 
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import pages.CommonPage;
 import pages.HealthSeniorPage;
+import utils.BrowserUtils;
 
 public class HealthSeniorSteps implements CommonPage {
     HealthSeniorPage page;
 
     public HealthSeniorSteps() {
         page = new HealthSeniorPage();
+    }
+
+    @When("I click on {string} button")
+    public void i_click_on_button(String btn) {
+        BrowserUtils.getDriver().findElement(By.xpath(String.format(XPATH_TEMPLATE_TEXT_CONTAINS, btn))).click();
+    }
+
+    @Then("Verify pictures are displayed under header above")
+    public void verifyPicturesAreDisplayedUnderHeaderAbove() {
+        for (WebElement each : page.pictures) {
+            BrowserUtils.assertTrue(each.isDisplayed());
+        }
+    }
+
+    @Then("Verify {string} under name is displayed")
+    public void verifyUnderNameIsDisplayed(String txt) {
+        BrowserUtils.getDriver().findElement(By.xpath(String.format(XPATH_TEMPLATE_TEXT, txt))).click();
+    }
+
+    @Then("Verify text under name is displayed")
+    public void verifyTextUnderNameIsDisplayed() {
+        for (WebElement each : page.textUnderPic) {
+            BrowserUtils.assertTrue(each.isDisplayed());
+        }
+    }
+
+    @Then("Verify {string} is displayed")
+    public void verifyIsDisplayed(String txtUnderName) {
+        BrowserUtils.getDriver().findElement(By.xpath(String.format(XPATH_TEMPLATE_NUR, txtUnderName))).click();
+    }
+
+    @Then("Verify Online special buttons are displayed")
+    public void verifyOnlineSpecialButtonsAreDisplayed() {
+        for (WebElement each : page.onlineSpecialBtn) {
+            BrowserUtils.assertTrue(each.isDisplayed());
+        }
     }
 }
