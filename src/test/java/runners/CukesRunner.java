6c4a87e2fc351fcc42f9248d4eee2c1b92a892e3
Nurik <nicksbastion@yahoo.com>
2023-10-28 18:30:15 -0400

    blank

--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
         stepNotifications = true,
         dryRun = false /*if true, it will only run all feature steps not implemented and implement them*/
 
-        ,tags = "@US01"
+        ,tags = "@US-12.1"
 
 
         //keywords
