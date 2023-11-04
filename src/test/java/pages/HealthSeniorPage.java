bb1eb63c029cbc2254818ef0d1ba3f566f7b29cf
Nurik <nicksbastion@yahoo.com>
2023-11-04 17:46:26 -0400

    @AS-73 whis is @US23 is fixed

--- src/test/java/pages/HealthSeniorPage.java
+++ src/test/java/pages/HealthSeniorPage.java
@@ -37,7 +37,7 @@ public class HealthSeniorPage {
     public WebElement shopNowBtn3;
     @FindBy(xpath = "(//span[@class='btn-cta-text'])[4]")
     public WebElement shopNowBtn4;
-    @FindBy(xpath = "//u[normalize-space()='Click here.']")
+    @FindBy(xpath = "//div[@class='legal']//u[contains(text(),'Click here.')]")
     public WebElement clickHereBtn;
     @FindBy(xpath = "//div[@class='vid-img-overlay row no-gutters d-flex align-items-center align-self-center']")
     public WebElement videoImg;
--- src/test/java/step_definitions/HealthSeniorSteps.java
+++ src/test/java/step_definitions/HealthSeniorSteps.java
@@ -77,18 +77,21 @@ public class HealthSeniorSteps implements CommonPage {
 
     @When("I scroll up")
     public void iScrollUp() throws InterruptedException {
-//        Actions actions = new Actions(BrowserUtils.getDriver());
-//        actions.sendKeys(Keys.PAGE_UP).build().perform();
+        Actions actions = new Actions(BrowserUtils.getDriver());
+        actions.sendKeys(Keys.PAGE_UP).build().perform();
 //        JavascriptExecutor js = (JavascriptExecutor) BrowserUtils.getDriver();
 //        // Scroll up by a certain pixel amount (you can adjust the value as needed)
 //        js.executeScript("window.scrollBy(0, -250);");
     }
 
+
     @When("I click {string} button")
     public void iClickButton(String button) throws InterruptedException {
-      BrowserUtils.actionPageUP(page.clickHereBtn);
+        Actions actions = new Actions(BrowserUtils.getDriver());
+        actions.sendKeys(Keys.PAGE_UP).build().perform();
       BrowserUtils.click(page.clickHereBtn);
-
+        CucumberLogUtils.logPass("Clicked on button", true);
+BrowserUtils.switchToNewWindow();
 
     }
 
@@ -228,6 +231,8 @@ public class HealthSeniorSteps implements CommonPage {
         BrowserUtils.isDisplayed(page.adtEmblem);
         CucumberLogUtils.logPass("Image is displayed", true);
     }
+
+
 }
 
 
